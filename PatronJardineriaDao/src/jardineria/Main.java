3347d73302799190c41891af4d21a0d0c8bff3bc
Jame Ops <Jame Ops@DESKTOP-DRQLDNL>
2020-11-02 20:15:41 +0100

    Actualizado

--- PatronJardineriaDao/src/jardineria/Main.java
+++ PatronJardineriaDao/src/jardineria/Main.java
@@ -5,7 +5,7 @@ public class Main {
 		
 	Cliente builder = new Cliente("Dario","Jaime","Moreno",601543192,"Si","Calle1","Calle2","Zaragoza","Ourense","España",50002,null,66666);
 	
-	Pedido builderPedido= new Pedido(null, null, null, "En camino", null, null);
+	Pedido builderPedido= new Pedido(null, null, null, "LLegando", null, null);
 	System.out.println(builder);
 	System.out.println(builderPedido);
 	}
